2c889505a5a8d0f991e154f29a03b1e314aba841
LONGMANKE <mburunjoroge0@gmail.com>
2024-01-21 02:01:38 +0300

    added the APIResponse in helper and mapped the same in the ICustomer service

---
+++ LearnApi/Helper/APIResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace LearnAPI.Helper
+{
+    public class APIResponse
+    {
+        public int ResponseCode { get; set; }
+        public int Result { get; set; }
+        public int Errormessage { get; set; }
+    }
+}
